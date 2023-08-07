27573cafb95301d91b056abc7b2fe25c9c365b68
gamesman36 <gamesman36@outlook.com>
2023-08-07 14:48:50 +0200

    Random initial state

--- Bearoff/Program.cs
+++ Bearoff/Program.cs
@@ -2,12 +2,14 @@
 {
     internal class Program
     {
+        static readonly Random random = new Random();
         static void Main(string[] args)
         {
             var die1 = new Die();
             var die2 = new Die();
+            var checkerCounts = new List<int>();
+            checkerCounts = CreateBoard(checkerCounts);
             ShowRolledDice(die1, die2);
-            var checkerCounts = new List<int>() { 0, 0, 0, 3, 5, 7 };
             var board = new Homeboard(checkerCounts);
         }
 
@@ -21,6 +23,29 @@
                 : $"{roll2}-{roll1}";
 
             Console.WriteLine($"The roll is {rollString}.");
+        }
+
+        static List<int> CreateBoard(List<int> checkerCounts)
+        {
+            int checkersToDistribute = 15;
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (checkersToDistribute > 0)
+                {
+                    int maxCheckers = Math.Min(checkersToDistribute, 5);
+                    int numberOfCheckers = random.Next(1, maxCheckers + 1);
+                    int remainder = checkersToDistribute - numberOfCheckers;
+                    checkerCounts.Add(numberOfCheckers);
+                    checkersToDistribute = remainder;
+                }
+
+                else
+                {
+                    checkerCounts.Add(0);
+                }
+            }
+            return checkerCounts;
         }
     }
 }
